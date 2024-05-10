70537c062519e047fd29cf241b05ab06197fcff5
Maria <claudmaria1988@gmail.com>
2024-05-10 11:53:41 +0800

    first commit

---
+++ src/components/div.vue
@@ -0,0 +1,27 @@
+<template>
+	<div v-for="(tag,index) in tags" :key="index">
+	        {{tag}}
+	        <a @click.prevent="removeTag(index)" href="#">&times;</a>
+	    </div>
+	    <hr />
+	    {{newTag}}
+	    <input 
+	        type="text" 
+	        v-model.trim="newTag"
+	        @keydown.enter="addNewTag"
+	        @keydown.delete="removeLastTag"
+	        @keydown.tab.prevent="addNewTag"  
+	        :class="{'tag-exists': isTagExists }"
+	    />
+</template>
+
+<script setup lang="ts">
+defineProps<{ 
+	tags: string[];
+	removeTag: (index: any) => void;
+	newTag: string;
+	addNewTag: () => void;
+	removeLastTag: () => void;
+	isTagExists: boolean;
+}>()
+</script>
